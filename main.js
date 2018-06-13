0118ddf2accf2e6deb0c5342b92860974cc20f13
Eagle Labs <eaglelabs@Lab1-MacPro2.local>
2018-06-13 10:45:51 +0100

    added bootstrap and jquery functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#trlogo").hide()
+});
+$(document).ready(function(){
+    $("#carouselExampleControls").animate({
+        left: '250px'
+        });
+        
+});
